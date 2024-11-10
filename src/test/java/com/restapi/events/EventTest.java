d1119903a05b15bf800582df6cb5533c72a50462
DrumJ <tmdgh717@naver.com>
2024-11-10 11:36:38 +0900

    매개변수를 이용한 테스트

--- src/test/java/com/restapi/events/EventTest.java
+++ src/test/java/com/restapi/events/EventTest.java
@@ -1,6 +1,12 @@
 package com.restapi.events;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -93,5 +99,71 @@ class EventTest {
         //then
         assertFalse(event.isOffline());
 
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0, true",
+            "100, 0, false",
+            "0, 100, false"
+    })
+    void freeTestWithParams(int basePrice, int maxPrice, boolean isFree) throws Exception {
+        //given
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+        //when
+        event.update();
+
+        //then
+        assertEquals(event.isFree(),isFree);
+    }
+
+    @ParameterizedTest
+    @MethodSource("paramsForTestFree")
+    void freeTestWithParams_typeSafe(int basePrice, int maxPrice, boolean isFree) throws Exception {
+        //given
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+        //when
+        event.update();
+
+        //then
+        assertEquals(event.isFree(),isFree);
+    }
+
+    private static Stream<Arguments> paramsForTestFree() {
+        return Stream.of(
+                Arguments.of(0, 0, true),
+                Arguments.of(100, 0, false),
+                Arguments.of(0, 100, false),
+                Arguments.of(100, 200, false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("paramsForTestOffline")
+    void testOfflineWithParams(String location, boolean isOffline) {
+        //given
+        Event event = Event.builder()
+                .location(location)
+                .build();
+
+        //when
+        event.update();
+
+        //then
+        assertEquals(event.isOffline(),isOffline);
+    }
+
+    private static Object[] paramsForTestOffline() {
+        return new Object[]{
+                new Object[] {"강남",true},
+                new Object[] {null, false},
+                new Object[] {"   ", false}
+        };
     }
 }
